4058026bd19aad8092614ab0ed34052f614e841f
Tejas Rajan <rajan.tejas@yahoo.com>
2022-08-16 13:37:25 +0530

    output

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -6,14 +6,14 @@ public class Customer {
     private String email;
     private long mobile;
     private boolean isPremium;
-    Customer(){
+    public Customer(){
         name = null;
         address = null;
         email = null;
         mobile = 0;
         isPremium = false;
     }
-    Customer(String name, String address, String email, long mobile, boolean isPremium){
+    public Customer(String name, String address, String email, long mobile, boolean isPremium){
         this.name = name;
         this.address = address;
         this.email = email;
--- src/main/CustomerImpl.java
+++ src/main/CustomerImpl.java
@@ -3,4 +3,15 @@ package main;
 import data.Customer;
 
 public class CustomerImpl {
+    public static void main(String[] args) {
+
+        Customer object1 = new Customer("Tejas", "Delhi", "tejas@yahoo.com", 9876543210L, true);
+        Customer object2 = new Customer();
+        Customer temp = object1;
+        object1 = object2;
+        object2 = temp;
+        object1.displayCustomer();
+        System.out.println();
+        object2.displayCustomer();
+    }
 }
