29ff493f8a1625216f3d32a070a5b049c180ce09
Tejas Rajan <rajan.tejas@yahoo.com>
2022-08-16 13:20:26 +0530

    attributes

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -1,4 +1,9 @@
 package data;
 
 public class Customer {
+    private String name;
+    private String address;
+    private String email;
+    private long mobile;
+    private boolean isPremium;
 }
