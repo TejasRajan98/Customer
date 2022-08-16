59fd913fb0316bf4eee78bf04f593f1122ad1a4d
Tejas Rajan <rajan.tejas@yahoo.com>
2022-08-16 13:23:55 +0530

    constructors

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -6,4 +6,18 @@ public class Customer {
     private String email;
     private long mobile;
     private boolean isPremium;
+    Customer(){
+        name = null;
+        address = null;
+        email = null;
+        mobile = 0;
+        isPremium = false;
+    }
+    Customer(String name, String address, String email, long mobile, boolean isPremium){
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this. mobile = mobile;
+        this.isPremium = isPremium;
+    }
 }
